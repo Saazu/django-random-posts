a1c0399eaf078ce044385ccbc4b554355b31c4e9
alfreddelle <alfred.delle>
2024-05-29 15:47:41 +0000

    Add tests

--- pages/tests.py
+++ pages/tests.py
@@ -1,6 +1,7 @@
 from django.test import Client, TestCase
 from django.urls import reverse
 from .models import Post
+from django.contrib.messages import get_messages
 
 
 class PostTestCase(TestCase):
@@ -12,16 +13,20 @@ class PostTestCase(TestCase):
     def test_home_view(self):
         response = self.client.get(self.home_url)
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "posts/home.html")
+        self.assertTemplateUsed(response, "home.html")
 
     def test_success_view_with_no_posts(self):
         response = self.client.get(self.success_url)
         self.assertEqual(response.status_code, 200)
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(str(messages[0]), "No posts available yet.")
         self.assertTemplateUsed(response, "success.html")
-        self.assertContains(response, "No posts available yet.")
 
     def test_success_view_with_posts(self):
-        post = Post.objects.create(title="Test Post", body="This is a test post.")
+        post = Post.objects.create(
+            userId=1, title="Test Post", body="This is a test post."
+        )
         response = self.client.get(self.success_url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "success.html")
